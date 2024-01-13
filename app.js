2d76b60b93778dc5167b5f76ad6f43f581b29efe
Michael Chandler <macjr93@yahoo.com>
2024-01-12 22:35:55 -0800

    setup basic creation

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-const button = document.querySelector('.changes');
+// - working code
+// - click button to add button
 const container = document.getElementById("container");
+const changes = document.querySelector('.changes');
+const remover = document.querySelectorAll('.remover');
 
-for (let i = 0; i < 100; i++) {
-    /*First iliteration: button = a new DOM object, second iliteration:
-      button = a new DOM object, etc.*/
+changes.addEventListener("click", () => {
     let button = document.createElement("button");
     container.appendChild(button);
-    button.innerText = "Button";
-}
-
-// if (button.clicked == true) {
-//     console.log('it worked!');
-// };
-
-button.addEventListener("click", () => {
+    button.innerText = "delete";
+    button.className = "remover";
     console.log('it worked!');
 });
+
+// remover.addEventListener("click", () => {
+//     console.log('it worked!');
+// });
+
+// - reference code
+
+
+// for (let i = 0; i < 100; i++) {
+//     /*First iliteration: button = a new DOM object, second iliteration:
+//       button = a new DOM object, etc.*/
+//     let button = document.createElement("button");
+//     container.appendChild(button);
+//     button.innerText = "Button";
+// }
