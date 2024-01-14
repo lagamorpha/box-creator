7fd19be4d7d55c27cdb4a2de5cf81c625313c5e2
Michael Chandler <macjr93@yahoo.com>
2024-01-13 21:24:14 -0800

    troubleshoot generated buttons

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 // - click button to add button
 const container = document.querySelector("#container");
 const spawner = document.querySelector('#spawner');
-// const remover = document.querySelector('.remover');
+const testButotn = document.querySelector('#test');
+const buttons = document.querySelectorAll('button');
 
 spawner.addEventListener('click', function spawn() {
     let btn = document.createElement('button');
     container.appendChild(btn);
     btn.innerText = 'delete';
-    btn.className = 'remover';
+    // btn.className = 'remover';
     console.log('it worked!');
 });
 
-// remover.addEventListener('click', function () {
-//     console.log('it worked!');
-// });
+for (let button of buttons) {
+    button.addEventListener('dblclick', function remove() {
+        console.log('it worked!');
+    });
+}
