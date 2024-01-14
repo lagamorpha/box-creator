0a08cb44dcd5c12b29c102e9b37fae59b0a14c30
Michael Chandler <macjr93@yahoo.com>
2024-01-13 21:31:27 -0800

    setup box moving logic

--- app.js
+++ app.js
@@ -18,3 +18,23 @@ for (let button of buttons) {
         console.log('it worked!');
     });
 }
+
+// move boxes using absolute position
+window.addEventListener('keydown', function (e) {
+    switch (e.code) {
+        case 'ArrowUp':
+            console.log("UP!");
+            break;
+        case 'ArrowDown':
+            console.log("DOWN!");
+            break;
+        case 'ArrowLeft':
+            console.log("LEFT!");
+            break;
+        case 'ArrowRight':
+            console.log("RIGHT!");
+            break
+        default:
+            console.log("IGNORED!")
+    }
+})
