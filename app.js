9a8a877a51f752e6a8fee506088bfcde77a8a47e
Michael Chandler <macjr93@yahoo.com>
2024-01-15 21:27:47 -0800

    finish basic color changing behavior

--- app.js
+++ app.js
@@ -24,6 +24,15 @@ const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
+    console.log(r, g, b);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const makeOppoColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    console.log(r, g, b);
     return `rgb(${r}, ${g}, ${b})`;
 }
 
@@ -41,12 +50,16 @@ window.addEventListener('keydown', function (e) {
         case 'ArrowRight':
             console.log("RIGHT!");
             break
+        case 'Space':
+            console.log("SPACE!");
+            break
         default:
-            // const newColor = makeRandColor();
-            // for (let button of buttons) {
-            //     // document.body.style.buttons.backgroundColor = newColor;
-            //     // buttons.innerText = newColor;
-            // }
+            const newColor = makeRandColor();
+            const oppoColor = makeOppoColor();
+            for (let button of buttons) {
+                button.style.backgroundColor = newColor;
+                button.style.color = oppoColor;
+            }
             console.log("IGNORED!")
     }
 })
