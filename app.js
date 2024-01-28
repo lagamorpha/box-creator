78d444b47bcebcdb84d4a887296f71028ba43da1
Michael Chandler <macjr93@yahoo.com>
2024-01-27 18:13:42 -0800

    add delete event listener and color inverter code

--- app.js
+++ app.js
@@ -5,21 +5,7 @@ const spawner = document.querySelector('#spawner');
 const testButotn = document.querySelector('#test');
 const buttons = document.querySelectorAll('button');
 
-spawner.addEventListener('click', function spawn() {
-    let btn = document.createElement('button');
-    container.appendChild(btn);
-    btn.innerText = 'delete';
-    // btn.className = 'remover';
-    console.log('it worked!');
-});
-
-for (let button of buttons) {
-    button.addEventListener('dblclick', function remove() {
-        console.log('it worked!');
-    });
-}
-
-// move boxes using absolute position, change color if non-functional button
+// random color generator and inverter
 const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
@@ -29,37 +15,51 @@ const makeRandColor = () => {
 }
 
 const makeOppoColor = () => {
-    const r = Math.floor(Math.random() * 255);
-    const g = Math.floor(Math.random() * 255);
-    const b = Math.floor(Math.random() * 255);
-    console.log(r, g, b);
-    return `rgb(${r}, ${g}, ${b})`;
+    let rgb = makeRandColor();
+
+    rgb = rgb.substring(4, rgb.length - 1)
+        .replace(/ /g, '')
+        .split(',');
+
+    console.log(rgb);
 }
 
-window.addEventListener('keydown', function (e) {
-    switch (e.code) {
-        case 'ArrowUp':
-            console.log("UP!");
-            break;
-        case 'ArrowDown':
-            console.log("DOWN!");
-            break;
-        case 'ArrowLeft':
-            console.log("LEFT!");
-            break;
-        case 'ArrowRight':
-            console.log("RIGHT!");
-            break
-        case 'Space':
-            console.log("SPACE!");
-            break
-        default:
-            const newColor = makeRandColor();
-            const oppoColor = makeOppoColor();
-            for (let button of buttons) {
-                button.style.backgroundColor = newColor;
-                button.style.color = oppoColor;
-            }
-            console.log("IGNORED!")
-    }
-})
+// window.addEventListener('keydown', function (e) {
+//     switch (e.code) {
+//         case 'ArrowUp':
+//             console.log("UP!");
+//             break;
+//         case 'ArrowDown':
+//             console.log("DOWN!");
+//             break;
+//         case 'ArrowLeft':
+//             console.log("LEFT!");
+//             break;
+//         case 'ArrowRight':
+//             console.log("RIGHT!");
+//             break
+//         case 'Space':
+//             console.log("SPACE!");
+//             break
+//         default:
+//             const newColor = makeRandColor();
+//             const oppoColor = makeOppoColor();
+//             for (let button of buttons) {
+//                 button.style.backgroundColor = newColor;
+//                 button.style.color = oppoColor;
+//             }
+//             console.log("IGNORED!")
+//     }
+// });
+
+spawner.addEventListener('click', function spawn() {
+    let btn = document.createElement('button');
+    container.appendChild(btn);
+    btn.innerText = 'delete';
+    // btn.style.color = makeRandColor();
+    btn.style.backgroundColor = makeOppoColor();
+});
+
+container.addEventListener('click', function (e) {
+    e.target.nodeName === 'BUTTON' && e.target.remove();
+});
