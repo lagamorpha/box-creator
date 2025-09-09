2ea5ed3290a9a773ee4d4871f5d3c09d5a1a3f00
Michael Chandler <macjr93@yahoo.com>
2025-09-09 14:01:34 -0700

    recover color changing function

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ const destroyer = document.getElementsByClassName('destroyer');
 //     console.log("Click");
 // });
 
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    console.log(r, g, b);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const makeOppoColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    console.log(r, g, b);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 creator.addEventListener("click", () => {
     let button = document.createElement("button");
     container.appendChild(button);
@@ -16,12 +32,12 @@ creator.addEventListener("click", () => {
     button.className = "destroyer";
     button.style.color = makeRandColor();
     button.style.backgroundColor = makeOppoColor();
-    console.log('it worked!');
+    // console.log('it worked!');
 });
 
-destroyer.addEventListener("click", () => {
-    console.log('it worked!');
-});
+// destroyer.addEventListener("click", () => {
+//     console.log('it worked!');
+// });
 
 // container.addEventListener('click', function (e) {
 //     e.target.nodeName === 'BUTTON' && e.target.remove();
